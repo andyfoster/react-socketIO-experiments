b16e2395f5a595f494dc038afb29857f9b5d886a
andyfoster <andrewfosteris@gmail.com>
2017-08-04 21:14:16 +0700

    Add React and Babel

---
+++ components/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (<h1>Hello World from Reactive App</h1>);
+  }
+}
+
+// module.exports = App;
+export default App;
