dc994b76c1405abfe7a76cfa95b61df218d8ca56
andyfoster <andrewfosteris@gmail.com>
2017-08-04 21:54:19 +0700

    Add Socket.io and test the connection between Node server and React client

--- app-server.js
+++ app-server.js
@@ -5,5 +5,11 @@ var app = express();
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
 
-app.listen(3000);
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', (socket) => {
+  console.log("Connection: %s", socket.id);
+});
+
 console.log('Polling server is running on http://localhost:3000/');
--- components/app.js
+++ components/app.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 class App extends React.Component {
+
+  componentWillMount() {
+    this.socket = io('http://localhost:3000');
+    this.socket.on('connect', this.connect);
+  }
+
+  connect() {
+    alert("Connected: " + this.id);
+    console.log(this.id);
+  }
+
   render() {
-    return (<h1>Hello World from Reactive App</h1>);
+    return (<h1>Hello World from Socket App</h1>);
   }
 }
 
