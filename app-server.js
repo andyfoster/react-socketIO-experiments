60b3a1f00b11c8afe4d6e6365460a93874fb02eb
andyfoster <andrewfosteris@gmail.com>
2017-08-05 20:31:07 +0700

    Add a disconnect event and corresponding behaviour to let user know

--- app-server.js
+++ app-server.js
@@ -1,6 +1,7 @@
 var express = require('express');
-
 var app = express();
+
+var connections = [];
 
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
@@ -9,7 +10,14 @@ var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', (socket) => {
-  console.log("Connection: %s", socket.id);
+  socket.once('disconnect', () => {
+    connections.splice(conections.indexOf(socket), 1);
+    socket.disconnect();
+    console.log('Disconnected: %s sockets remaining', connections.length);
+  });
+
+  connections.push(socket);
+  console.log("Connection: %s sockets connected", connections.length);
 });
 
 console.log('Polling server is running on http://localhost:3000/');
--- components/app.js
+++ components/app.js
@@ -14,19 +14,25 @@ class App extends React.Component {
   componentWillMount() {
     this.socket = io('http://localhost:3000');
     this.socket.on('connect', this.connect.bind(this));
+    this.socket.on('disconnect', this.disconnect.bind(this));
   }
 
   connect() {
-    // console.log("Connected: " + this.id);
     this.setState({
       status: 'connected'
+    });
+  }
+
+  disconnect() {
+    this.setState({
+      status: 'disconnected'
     });
   }
 
   render() {
     return (
       <div>
-        <Header title="New Header" status={this.state.status}/>
+        <Header title="New Header" status={this.state.status} />
       </div>
     );
   }
